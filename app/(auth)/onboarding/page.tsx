2fdfec91920eeebe039b02d1c6e9adc01da133df
viet <viet.gs03@gmail.com>
2023-12-22 22:05:58 +0700

    first commit
    first commit project next js

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function Page() {
+    return (
+        <main>
+            <h1 className="head-text">onBoading</h1>
+        </main>
+    )
+}
+
+export default Page;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import Topbar from '@/components/shared/Topbar'
+import Leftsidebar from '@/components/shared/Leftsidebar'
+import Rightsidebar from '@/components/shared/Rightsidebar'
+import Bottombar from '@/components/shared/Bottombar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title:'Threads',
+  description:'A next.js 13 meta Threads application'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar/>
+            <main className='flex flex-row'>
+              <Leftsidebar/>
+              <section className='main-container'>
+                <div className='w-full max-w-4xl'>
+                  {children}
+                </div>
+              </section>
+              <Rightsidebar/>
+            </main>
+          <Bottombar/>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
